7b7dfa016ac6bd20949432a43770f1040ec6a36c
Matthew Rocklin <mrocklin@gmail.com>
2011-11-08 08:32:04 -0600

    Initial population of repository

---
+++ mailFeed.py
@@ -0,0 +1,114 @@
+import imaplib
+from mailutils import *
+from time import sleep, time
+
+__all__ = ('dataFeed', 'mailFeed', 'attachmentsFeed', 'login')
+
+
+def stdLog(x):
+    print(x)
+def errLog(x):
+    print(x)
+
+default_username = ''
+default_password = ''
+default_host = 'imap.gmail.com'
+
+def login(username = default_username, password = default_password,
+        host = default_host, inbox="INBOX"):
+    '''Connects to IMAP server and enters inbox
+    Inputs:
+        username : "joe.schmoe"
+        password : "password1234"
+        host : "imap.gmail.com"
+
+    Outputs:
+        an mbox object imaplib.IMAP4_SSL(imap.gmail.com)
+        '''
+    stdLog('Logging into %s under username %s\n\n'%(host,username))
+    mbox = imaplib.IMAP4_SSL(host)
+    mbox.login(username, password)
+    mbox.create("DownloadedMails")
+    mbox.select(inbox)
+    return mbox
+
+def clearMessage(mbox, emailid):
+    '''Deletes message from imap server. Moves to "DownloadedMails" folder
+    mbox - a mailbox or server returned from login or IMAP4_SSL object
+    emailid - id (number or numeric string) of email to delete'''
+    mbox.copy(emailid, 'DownloadedMails')
+    mbox.store(emailid, "+FLAGS.SILENT", '(\\Deleted)')
+
+def dataFeed(waittime=60, **kwargs):
+    """ Generates a feed of raw email data from a mail server.
+    See login function for keyword arguments to specify server
+    Creates a generator/infinite list
+
+    >>> feed = dataFeed(username='sam', password='123', host='imap.gmail.com')
+    >>> for raw_data in feed:
+    ...     print raw_data
+
+    See Also:
+        login
+        mailFeed
+        attachmentFeed
+    """
+    mbox = login(**kwargs)
+
+    lastCheckTime = time()
+    while(1):
+        try:
+            newEmails = getEmailsFromServer(mbox)
+            lastCheckTime = time()
+            for mail in newEmails:
+                yield mail
+        except:
+            errLog('Email Login Failed - Retrying')
+            mbox = login() #maybe our authentication wore out?
+
+        # Compute sleep time
+        timeSinceLastCheck = time() - lastCheckTime
+        sleepTime = waittime - timeSinceLastCheck
+        if sleeptime <= 0:
+            sleeptime = 0
+        sleep(sleepTime)
+
+def getEmailsFromServer(mbox):
+    '''Fetch and return all new email from an mbox server
+    Returns raw e-mail data. Parse using mailFromData function'''
+
+    mbox.select('INBOX')
+
+    # Collect the IDs of all emails in the inbox
+    resp, items = mbox.search(None, 'ALL')
+    items = items[0].split()
+
+    dataPackets = []
+    for emailid in items:
+        response, data = mbox.fetch(emailid, "(RFC822)")
+
+        if response != 'OK':
+            errlog('Failed on message '+str(emailid))
+        else:
+            dataPackets.append(data)
+            clearMessage(mbox, emailid) # remove email from online inbox
+
+    return dataPackets
+
+def mailFeed(*args, **kwargs):
+    """ See runs mailFromData on dataFeed stream
+
+    See Also:
+        dataFeed
+        """
+    return (mailFromData(data) for data in dataFeed(*args, **kwargs))
+
+def attachmentsFeed(*args, **kwargs):
+    """ See runs getAttachments on mailFeed stream
+
+    See Also:
+        dataFeed
+        """
+    return (getAttachments(mail) for mail in mailFeed(*args, **kwargs))
+
+
---
+++ mailutils.py
@@ -0,0 +1,34 @@
+import email
+
+def addrFromMail(mail):
+    '''Scrapes the senders email address from the mail message'''
+    return email.Utils.parseaddr(mail['From'])[1].lower()
+
+def mailFromData(data):
+    '''Safely obtains the mail object from the data sent by the imap server'''
+    email_body = data[0][1]
+    mail = email.message_from_string(email_body)
+    return mail
+
+counter = 0
+def getAttachments(mail):
+    '''
+    Given a mail object return a list of attachments
+
+    Returns list of (filename, data payload) pairs
+    '''
+    if mail.get_content_maintype() != 'multipart':
+       return []
+
+    results = []
+    for part in mail.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part.get('Content-Disposition') is None:
+            continue
+        filename = part.get_filename()
+        if not filename: #anonymous file
+            filename = 'part-%03d%s' % (counter, 'bin')
+            counter += 1
+        results.append( (filename, part.get_payload(decode=True)) )
+    return results
